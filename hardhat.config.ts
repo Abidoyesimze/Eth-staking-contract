01365b5a7df9fe29692b919168307698950f6b35
Abidoyesimze <similoluwaeyitayoabidoye@gmail.com>
2024-08-31 03:29:42 +0100

    second-commit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,14 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    // for testnet
+    'lisk-sepolia': {
+      url: 'https://rpc.sepolia-api.lisk.com',
+      accounts: [process.env.PRIVATE_KEY as string],
+      gasPrice: 1000000000,
+    },
+  },
 };
 
 export default config;
